50c54126164cb6e66b6621a3f43048d3b8c8ecda
Jonathan Rodrigues de Oliveira <jonathan.oliveira@db1.com.br>
2014-06-18 15:37:22 -0300

    Primeiro commit

---
+++ www/js/app.js
@@ -0,0 +1,72 @@
+angular.module('netanimations', ['ionic', 'netanimations.controllers'])
+
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/search.html"
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            })
+
+            .state('app.about', {
+                url: "/about",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/about.html",
+                        controller: 'AboutCtrl'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/app/playlists');
+    });
